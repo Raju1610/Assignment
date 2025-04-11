d841bd9d37b21819b1be178525757c68def325ab
Rajesh b <raju161055@gmail.com>
2025-04-11 10:35:10 +0530

    Assignment3

---
+++ src/org/sprac/cretio/crm/basics/Employees.java
@@ -0,0 +1,21 @@
+package org.sprac.cretio.crm.basics;
+
+public class Employees {
+	
+	String name1 ="Bharath";
+	int EmpId1 = 1234;
+	
+	String name2 = "ABC";
+	int EmpId2 = 1235;
+	
+	String name3 = "XYZ";
+	int EmpId3 = 1236;
+			
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
